af93d58f41c2e17b5734fdc0ff12a946442496a3
Chris Norman <chris.norman2@googlemail.com>
2022-07-31 16:33:43 +0100

    Updated the example.

--- example/dart_tolk_example.dart
+++ example/dart_tolk_example.dart
@@ -5,12 +5,13 @@ import 'package:dart_tolk/dart_tolk.dart';
 Future<void> main() async {
   final t = Tolk.fromPath('tolk.dll')..load();
   print('Loaded tolk.');
-  t.trySapi = true;
   print('Screen reader: ${t.currentScreenReader}.');
   print('Screen reader ${t.hasBraille ? "has" : "does not have"} braille.');
   print('Screen reader ${t.hasSpeech ? "has" : "does not have"} speech.');
+  t.trySapi = true;
+  print('Trying SAPI.');
   t.output('Hello from Tolk, speaking through dart.');
-  await Future<void>.delayed(Duration(seconds: 2));
+  await Future<void>.delayed(const Duration(seconds: 2));
   t.unload();
   print('Unloaded Tolk.');
 }
